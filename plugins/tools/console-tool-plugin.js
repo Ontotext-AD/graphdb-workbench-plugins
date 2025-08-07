534e68111afcf35215755a300213483d3b502c1e
Plamen Yordanov <plamen.yordanov@graphwise.ai>
2025-08-07 14:04:18 +0300

    GDB-12868 Add webpack and configure build
    ## What
    Add Webpack and configure build
    
    ## Why
    To have the ability to build package the plugins
    
    ## How
    - Added dependencies for webpack and babel, to be able to transpile and package the plugins
    - Added test plugins
    
    ## Testing
    n/a

---
+++ plugins/tools/console-tool-plugin.js
@@ -0,0 +1,9 @@
+// TODO: for testing purposes. Must be removed
+export function register(registry) {
+  const definition = {
+    label: 'Console Tool',
+    action: () => console.log('New tool action')
+  };
+  console.log('%cRegister', 'background: yellow', definition.label);
+  registry.add('tools', definition);
+}
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const glob = require('glob');
+
+// Find all JS files in the plugins directory.
+// Map them to an object entry in order to keep the file structure.
+// This way files can be imported directly using their path.
+const entry = glob.sync('./plugins/**/*.js')
+    .reduce((acc, file) => {
+      const entryName = file.replace('./plugins', '').replace('.js', '');
+      acc[entryName] = file;
+      return acc;
+    }, {});
+
+module.exports = {
+  mode: 'production',
+  entry,
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    filename: '[name].js',
+    libraryTarget: 'module',
+    globalObject: 'window'
+  },
+  experiments: {
+    outputModule: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        { 
+          from: './plugins-manifest.json',
+          to: 'plugins-manifest.json'
+        }
+      ]
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js']
+  }
+};
