99df656e4c35b41f1e3c4d0baed98182cccf6eb1
Desi Borisova <158429017+DesiBorisova@users.noreply.github.com>
2025-09-02 17:10:21 +0300

    GDB-12852 - specify if "Add missing Namespaces" should be checked (#8)
    Co-authored-by: desislava.borisova <desislava.borisova@graphwise.ai>

--- plugins/guides/ttyg/sparql-search-method-enable-sparql-query.js
+++ plugins/guides/ttyg/sparql-search-method-enable-sparql-query.js
@@ -1,17 +1,22 @@
 const step = {
   guideBlockName: 'sparql-search-method-enable-sparql-query',
   getSteps: (options, _services) => {
-    return [
+    const steps = [
       {
         guideBlockName: 'ttyg-sparql-method-sparql-query-select', options: {...options}
       },
       {
         guideBlockName: 'ttyg-sparql-copy-query-text', options: {...options}
-      },
-      {
-        guideBlockName: 'ttyg-sparql-click-add-namespaces', options: {...options}
       }
     ];
+
+    if (options.addMissingNamespaces) {
+      steps.push({
+        guideBlockName: 'ttyg-sparql-click-add-namespaces', options: {...options}
+      });
+    }
+
+    return steps;
   }
 };
 
