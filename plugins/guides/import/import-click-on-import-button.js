235f17ae849f66300d6b979dc754492aef510f54
DesiBorisova <desislava.borisova@ontotext.com>
2025-10-01 16:59:29 +0300

    GDB-13153 - add translations for import steps
    GDB-13153 - add guides utils docs
    
    GDB-13153 - remove photos from resources directory
    
    GDB-13153 - remove main menu import photo from resources directory

--- plugins/guides/import/import-click-on-import-button.js
+++ plugins/guides/import/import-click-on-import-button.js
@@ -1,17 +1,37 @@
-import * as Utils from '../utils.js';
+const DEFAULT_TITLE = 'guide.step-action.import-file';
+const CONTENT = 'guide.step_plugin.import_rdf_file.import-settings.import.button.content';
+const UNEXPECTED_ERROR = 'guide.unexpected.error.message';
 
+/**
+ * @name import-click-on-import-button
+ * @memberof module:Interactive Guide
+ *
+ * @description
+ *
+ * The `import-click-on-import-button` step guides the user to click the import button in the import settings dialog of GraphDB.<br>
+ * <img src="resources/guides/import/import-click-on-import-button.png" style="height:200px; border: solid; border-width:1px"/>
+ *
+ * This step can be configured using the common options defined in [Options](#.Options).
+ *
+ * @example
+ * ```JSON
+ * {
+ *   "guideBlockName": "import-click-on-import-button"
+ * }
+ * ```
+ */
 const step = {
   guideBlockName: 'import-click-on-import-button',
   getSteps: (options, services) => {
     const GuideUtils = services.GuideUtils;
+    const translate = services.translate;
     const importSettingsButtonSelector = GuideUtils.getGuideElementSelector('import-settings-import-button');
     return [
       {
         guideBlockName: 'clickable-element',
         options: {
-          content: 'guide.step_plugin.import_rdf_file.import-settings.import.button.content',
-          // If mainAction is set the title will be set automatically
-          ...(options.mainAction ? {} : {title: Utils.IMPORT_FILE_DEFAULT_STEP_TITLE}),
+          content: translate(step.translationBundle, CONTENT),
+          ...(options.title ? {} : {title: translate(step.translationBundle, DEFAULT_TITLE)}),
           placement: 'top',
           class: 'import-settings-import-file-button',
           ...options,
@@ -23,7 +43,7 @@ const step = {
           beforeShowPromise: () => services.GuideUtils.deferredShow(300)()
             .then(() => GuideUtils.getOrWaitFor(importSettingsButtonSelector, 3)
               .catch((error) => {
-                services.toastr.error(services.$translate.instant('guide.unexpected.error.message'));
+                services.toastr.error(translate(step.translationBundle, UNEXPECTED_ERROR));
                 return Promise.reject(error);
               })
             ),
@@ -32,6 +52,18 @@ const step = {
         }
       }
     ];
+  },
+  translationBundle: {
+    en: {
+      [DEFAULT_TITLE]: 'Import file',
+      [CONTENT]: 'Click on the <b>Import</b> button.',
+      [UNEXPECTED_ERROR]: 'The guide was cancelled due to an unexpected error. Please run the guide again and if the problem persists contact the support.'
+    },
+    fr: {
+      [DEFAULT_TITLE]: 'Importer un fichier',
+      [CONTENT]: 'Cliquez sur le bouton <b>Importer</b>.',
+      [UNEXPECTED_ERROR]: 'Le guide a été annulé en raison d\'une erreur inattendue. Veuillez exécuter à nouveau le guide et si le problème persiste, contactez le support.'
+    }
   }
 };
 
--- plugins/guides/import/import-confirm-duplicate-files.js
+++ plugins/guides/import/import-confirm-duplicate-files.js
@@ -1,16 +1,35 @@
-import * as Utils from '../utils.js';
+const DEFAULT_TITLE = 'guide.step-action.import-file';
+const CONTENT = 'guide.step_plugin.import_rdf_file.confirm_duplicate_files_dialog.content';
 
+/**
+ * @name import-confirm-duplicate-files
+ * @memberof module:Interactive Guide
+ *
+ * @description
+ *
+ * The `import-confirm-duplicate-files` step guides the user to confirm overwriting duplicate files in the import dialog of GraphDB.<br>
+ * <img src="resources/guides/import/import-confirm-duplicate-files.png" style="height:200px; border: solid; border-width:1px"/>
+ *
+ * This step can be configured using the common options defined in [Options](#.Options).
+ *
+ * @example
+ * ```JSON
+ * {
+ *   "guideBlockName": "import-confirm-duplicate-files"
+ * }
+ * ```
+ */
 const step = {
   guideBlockName: 'import-confirm-duplicate-files',
   getSteps: (options, services) => {
     const GuideUtils = services.GuideUtils;
+    const translate = services.translate;
     return [
       {
         guideBlockName: 'clickable-element',
         options: {
-          content: 'guide.step_plugin.import_rdf_file.confirm_duplicate_files_dialog.content',
-          // If mainAction is set the title will be set automatically
-          ...(options.mainAction ? {} : {title: Utils.IMPORT_FILE_DEFAULT_STEP_TITLE}),
+          content: translate(step.translationBundle, CONTENT),
+          ...(options.title ? {} : {title: translate(step.translationBundle, DEFAULT_TITLE)}),
           placement: 'bottom',
           class: 'import-file-button',
           ...options,
@@ -29,6 +48,16 @@ const step = {
         }
       }
     ];
+  },
+  translationBundle: {
+    en: {
+      [DEFAULT_TITLE]: 'Import file',
+      [CONTENT]: 'Click on the <b>Yes</b> button to override the existing file.'
+    },
+    fr: {
+      [DEFAULT_TITLE]: 'Importer un fichier',
+      [CONTENT]: 'Cliquez sur le bouton <b>Oui</b> pour remplacer le fichier existant.'
+    }
   }
 };
 
