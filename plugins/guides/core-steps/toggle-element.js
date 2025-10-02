6411b6489f92f595044f1993f30ffb3d1172bc38
Plamen Yordanov <plamen.yordanov@graphwise.ai>
2025-10-02 11:17:17 +0300

    GDB-13142 Disallow the guide from continuing if toggleable elements are not clicked
    ## What
    Disallow the guide from continuing if toggleable elements are not clicked.
    
    ## Why
    There are scenarios, where you have a previous button, and you can go back and uncheck a checkbox/toggle button. This click advances the guide, but the checkbox/toggle button need to be checked
    
    ## How
    - Added new `toggle-element` step, which functions similarly to `clickable-element`. It also contains click event listener and doesn't allow the guide to proceed, if the input element is not checked. This effectively blocks deselecting of the element
    - Replaced `ttyg-sparql-method-enable` and `ttyg-sparql-click-add-namespaces` steps to be toggleable elements
    
    ## Testing
    n/a

---
+++ plugins/guides/core-steps/toggle-element.js
@@ -0,0 +1,69 @@
+import {BASIC_STEP} from '../config.js';
+import * as Utils from '../utils.js';
+
+/**
+ * @name toggleable-element
+ * @memberof module:Interactive Guide
+ *
+ * @description
+ * The Toggleable Element step is used to guide users to enable a specific toggleable element (like a switch or checkbox)
+ * on the interface. The step ensures that a user cannot go back to this step and deselect the toggleable element.
+ *
+ * Toggleable element example<br>
+ * <img src="resources/guides/core/toggle-element.png" style="height:200px; border: solid; border-width:1px"/><br>
+ *
+ * @example
+ * ```JSON
+ * {
+ *  "guideBlockName": "toggleable-element",
+ * }
+ * ```
+ */
+const step = {
+  // An element, which can be toggled via click
+  guideBlockName: 'toggle-element',
+  getStep: (options, pluginService) => {
+    const notOverridable = {
+      type: 'toggleable'
+    };
+
+    let stepHTMLElement;
+    const selector = options.toggleableElementSelector || options.elementSelector;
+
+    const toggleListener = (event) => {
+      if (!event.target.checked) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+    };
+    const stepDescription = {
+      ...BASIC_STEP,
+      advanceOn: {
+        selector,
+        event: 'click'
+      },
+
+      initPreviousStep: pluginService.GuideUtils.defaultInitPreviousStep,
+      ...options,
+      show: () => () => {
+        stepHTMLElement = document.querySelector(selector);
+        stepHTMLElement.addEventListener('click', toggleListener, true);
+      },
+      hide: () => () => {
+        if (stepHTMLElement) {
+          stepHTMLElement.removeEventListener('click', toggleListener);
+        }
+      },
+      ...notOverridable
+    };
+
+    if (!stepDescription.beforeShowPromise) {
+      stepDescription.beforeShowPromise = Utils.beforeShowPromise(pluginService, stepDescription.elementSelector, stepDescription.maxWaitTime);
+    }
+    return stepDescription;
+  }
+};
+
+export function register(registry) {
+  registry.add('guide.step', step);
+}
