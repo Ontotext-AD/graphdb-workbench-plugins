5fc692dc3f85b93c39a8c179f0d19e058526f838
Plamen Yordanov <plamen.yordanov@graphwise.ai>
2025-10-02 11:17:17 +0300

    GDB-13142 Disallow the guide from continuing if toggleable elements are not clicked
    ## What
    Disallow the guide from continuing if toggleable elements are not clicked.
    
    ## Why
    There are scenarios, where you have a previous button, and you can go back and uncheck a checkbox/toggle button. This click advances the guide, but the checkbox/toggle button need to be checked
    
    ## How
    - Added new `toggle-element` step, which functions similarly to `clickable-element`. It also contains click event listener and doesn't allow the guide to proceed, if the input element is not checked. This effectively blocks deselecting of the element
    - Replaced `ttyg-sparql-method-enable` and `ttyg-sparql-click-add-namespaces` steps to be toggleable elements
    
    ## Testing
    n/a

--- plugins/guides/ttyg/ttyg-sparql-click-add-namespaces.js
+++ plugins/guides/ttyg/ttyg-sparql-click-add-namespaces.js
@@ -1,27 +1,56 @@
-import * as Utils from '../utils.js';
-
+const TTYG_SPARQL_SEARCH_METHOD_DEFAULT_TITLE = 'guide.step-action.sparql-search-method';
+const ADD_MISSING_NAMESPACES = 'guide.step_plugin.sparql-search-method.add-missing-namespaces';
+/**
+ * @name ttyg-sparql-click-add-namespaces
+ * @memberof module:Interactive Guide
+ *
+ * @description
+ * This step is used to guide users to enable the "Add missing namespaces" option in the TTYG agent SPARQL query settings.
+ * It is a toggle step, which means that the user can enable the switch, but disabling will not be allowed.
+ *
+ * TTYG SPARQL add missing namespaces option<br>
+ * <img src="resources/guides/ttyg/ttyg-sparql-click-add-namespaces.png" style="height:200px; border: solid; border-width:1px"/><br>
+ *
+ * @example
+ * ```JSON
+ * {
+ *   guideBlockName: 'ttyg-sparql-click-add-namespaces',
+ * }
+ * ```
+ */
 const step = {
   guideBlockName: 'ttyg-sparql-click-add-namespaces',
-  getSteps: (options, services) => {
-    const GuideUtils = services.GuideUtils;
+  getSteps: function(options, pluginService) {
+    const GuideUtils = pluginService.GuideUtils;
+    const translate = pluginService.translate;
+    const title = options.title ? options.title : translate(this.translationBundle, TTYG_SPARQL_SEARCH_METHOD_DEFAULT_TITLE);
     const namespacesCheckbox = GuideUtils.getGuideElementSelector('add-missing-namespaces-input');
 
     return [
       {
-        guideBlockName: 'clickable-element',
+        guideBlockName: 'toggle-element',
         options: {
-          content: 'guide.step_plugin.sparql-search-method.add-missing-namespaces',
-          // If mainAction is set the title will be set automatically
-          ...(options.mainAction ? {} : {title: Utils.TTYG_SPARQL_SEARCH_METHOD_DEFAULT_TITLE}),
+          title,
+          content: translate(this.translationBundle, ADD_MISSING_NAMESPACES),
           class: 'enable-sparql-query',
           ...options,
           url: 'ttyg',
           elementSelector: GuideUtils.getGuideElementSelector('add-missing-namespaces-option'),
-          clickableElementSelector: namespacesCheckbox,
+          toggleableElementSelector: namespacesCheckbox,
           onNextValidate: () => Promise.resolve(GuideUtils.isChecked(namespacesCheckbox))
         }
       }
     ];
+  },
+  translationBundle: {
+    en: {
+      [TTYG_SPARQL_SEARCH_METHOD_DEFAULT_TITLE]: 'SPARQL search query method',
+      [ADD_MISSING_NAMESPACES]: 'Check to automatically add missing namespaces.'
+    },
+    fr: {
+      [TTYG_SPARQL_SEARCH_METHOD_DEFAULT_TITLE]: 'Méthode de recherche SPARQL',
+      [ADD_MISSING_NAMESPACES]: 'Cochez pour ajouter automatiquement les espaces de noms manquants.'
+    }
   }
 };
 
--- plugins/guides/ttyg/ttyg-sparql-method-enable.js
+++ plugins/guides/ttyg/ttyg-sparql-method-enable.js
@@ -1,26 +1,56 @@
-import * as Utils from '../utils.js';
+const TTYG_SPARQL_SEARCH_METHOD_DEFAULT_TITLE = 'guide.step-action.sparql-search-method';
+const ENABLE_TOGGLE = 'guide.step_plugin.sparql-search-method.enable-toggle';
 
+/**
+ * @name ttyg-sparql-method-enable
+ * @memberof module:Interactive Guide
+ *
+ * @description
+ * This step is used to enable the SPARQL search method in the TTYG plugin.
+ * It is a toggle step, which means that the user can enable the switch, but disabling will not be allowed.
+ *
+ * Enable SPARQL query search method<br>
+ * <img src="resources/guides/ttyg/ttyg-sparql-method-enable.png" style="height:200px; border: solid; border-width:1px"/><br>
+ *
+ * @example
+ * ```JSON
+ * {
+ *   guideBlockName: 'ttyg-sparql-method-enable',
+ * }
+ * ```
+ */
 const step = {
   guideBlockName: 'ttyg-sparql-method-enable',
-  getSteps: (options, services) => {
-    const GuideUtils = services.GuideUtils;
+  getSteps: function(options, pluginService) {
+    const GuideUtils = pluginService.GuideUtils;
+    const translate = pluginService.translate;
+    const title = options.title ? options.title : translate(this.translationBundle, TTYG_SPARQL_SEARCH_METHOD_DEFAULT_TITLE);
     const toggleSelector = GuideUtils.getGuideElementSelector('query-method-sparql_search-input');
     return [
       {
-        guideBlockName: 'clickable-element',
+        guideBlockName: 'toggleable-element',
         options: {
-          content: 'guide.step_plugin.sparql-search-method.enable-toggle',
-          // If mainAction is set the title will be set automatically
-          ...(options.mainAction ? {} : {title: Utils.TTYG_SPARQL_SEARCH_METHOD_DEFAULT_TITLE}),
+          title,
+          content: translate(this.translationBundle, ENABLE_TOGGLE),
           class: 'toggle-sparql-search',
           ...options,
           url: 'ttyg',
           elementSelector: GuideUtils.getGuideElementSelector('query-method-sparql_search'),
-          clickableElementSelector: toggleSelector,
+          toggleableElementSelector: toggleSelector,
           onNextValidate: () => Promise.resolve(GuideUtils.isChecked(toggleSelector))
         }
       }
     ];
+  },
+  translationBundle: {
+    en: {
+      [TTYG_SPARQL_SEARCH_METHOD_DEFAULT_TITLE]: 'SPARQL search query method',
+      [ENABLE_TOGGLE]: 'Click on the toggle to enable SPARQL search query method.'
+    },
+    fr: {
+      [TTYG_SPARQL_SEARCH_METHOD_DEFAULT_TITLE]: 'Méthode de recherche SPARQL',
+      [ENABLE_TOGGLE]: 'Cliquez sur le bouton pour activer la méthode de recherche SPARQL.'
+    }
   }
 };
 
