f190f1efd8e3ec3d4e6f1098c9516c5a4f24b3fe
Plamen Yordanov <plamen.yordanov@graphwise.ai>
2025-10-13 13:48:42 +0300

    GDB-13153 migrate TTYG SPARQL method
    ## What
    Migrate the TTYG SPARQL method translations
    
    ## Why
    To decouple translations from the workbench
    
    ## How
    Added translation bundles and documentation to SPARQL steps
    
    ## Testing
    n/a

--- plugins/guides/ttyg/sparql-search-method-enable-ontology-graph.js
+++ plugins/guides/ttyg/sparql-search-method-enable-ontology-graph.js
@@ -1,3 +1,33 @@
+/**
+ * @name sparql-search-method-enable-ontology-graph
+ * @memberof module:Interactive Guide
+ *
+ * @description
+ * This is a complex step that guides the user through enabling the option to provide an ontology in a named graph for the SPARQL search method in the TTYG interface.
+ * It includes steps to enable the option and type the name of the named graph that contains the ontology.
+ *
+ * Ontology select<br>
+ * <img src="resources/guides/ttyg/ttyg-sparql-method-ontology-select.png" style="height:200px; border: solid; border-width:1px"/><br>
+ *
+ * Type graph name<br>
+ * <img src="resources/guides/ttyg/sparql-search-method-type-graph-name.png" style="height:200px; border: solid; border-width:1px"/><br>
+ *
+ * Add missing namespaces<br>
+ * <img src="resources/guides/ttyg/ttyg-sparql-click-add-namespaces.png" style="height:200px; border: solid; border-width:1px"/><br>
+ *
+ * This step can be configured using the common options defined in [Options](#.Options). Additionally, it supports:
+ * @property {boolean} [options.addMissingNamespaces] - If true, an additional step will be included to click on the "Add missing namespaces" after typing the graph name.
+ *
+ * @example
+ * ```JSON
+ * {
+ *   "guideBlockName": "sparql-search-method-enable-ontology-graph",
+ *   "options": {
+ *     "addMissingNamespaces": true
+ *   }
+ * }
+ * ```
+ */
 const step = {
   guideBlockName: 'sparql-search-method-enable-ontology-graph',
   getSteps: (options, _services) => {
--- plugins/guides/ttyg/sparql-search-method-enable-sparql-query.js
+++ plugins/guides/ttyg/sparql-search-method-enable-sparql-query.js
@@ -1,3 +1,33 @@
+/**
+ * @name sparql-search-method-enable-sparql-query
+ * @memberof module:Interactive Guide
+ *
+ * @description
+ * This is a complex step that guides the user through enabling the option to provide a SPARQL CONSTRUCT query that fetches the ontology for the SPARQL search method in the TTYG interface.
+ * It includes steps to enable the option, select the SPARQL query option, copy the example query text, and optionally add missing namespaces.
+ *
+ * SPARQL query select<br>
+ * <img src="resources/guides/ttyg/ttyg-sparql-method-sparql-query-select.png" style="height:200px; border: solid; border-width:1px"/><br>
+ *
+ * Copy query text<br>
+ * <img src="resources/guides/ttyg/ttyg-sparql-copy-query-text.png" style="height:200px; border: solid; border-width:1px"/><br>
+ *
+ * Add missing namespaces<br>
+ * <img src="resources/guides/ttyg/ttyg-sparql-click-add-namespaces.png" style="height:200px; border: solid; border-width:1px"/><br>
+ *
+ * This step can be configured using the common options defined in [Options](#.Options). Additionally, it supports:
+ * @property {boolean} [options.addMissingNamespaces] - If true, an additional step will be included to click on the "Add missing namespaces" after copying the query text.
+ *
+ * @example
+ * ```JSON
+ * {
+ *   "guideBlockName": "sparql-search-method-enable-sparql-query",
+ *   "options": {
+ *     "addMissingNamespaces": true
+ *   }
+ * }
+ * ```
+ */
 const step = {
   guideBlockName: 'sparql-search-method-enable-sparql-query',
   getSteps: (options, _services) => {
