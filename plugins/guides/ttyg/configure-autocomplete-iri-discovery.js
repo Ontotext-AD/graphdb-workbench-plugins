a217da0eb79282e5ac1dc3129add32dcd8c0b183
Plamen Yordanov <plamen.yordanov@graphwise.ai>
2026-01-06 12:06:15 +0200

    GDB-13539 add steps for autocomplete IRI discovery
    ## What
    Add steps to guide users in enabling or disabling the autocomplete IRI discovery feature in the TTYG interface.
    
    ## Why
    To enhance user experience by providing clear guidance on configuring the autocomplete IRI discovery feature.
    
    ## How
    Created the new steps in separate files and documented them
    
    ## Testing
    n/a

---
+++ plugins/guides/ttyg/configure-autocomplete-iri-discovery.js
@@ -0,0 +1,78 @@
+const TTYG_DEFAULT_TITLE = 'menu.ttyg.label';
+const DISABLE_TOGGLE = 'guide.step_plugin.autocomplete.iri-discovery.disable-toggle';
+const ENABLE_TOGGLE = 'guide.step_plugin.autocomplete.iri-discovery.enable-toggle';
+
+/**
+ * @name configure-autocomplete-iri-discovery
+ * @memberof module:Interactive Guide
+ *
+ * @description
+ * This step guides the user to enable or disable the autocomplete IRI discovery feature in the TTYG interface.
+ * It highlights the toggle element and provides instructions on how to proceed.
+ *
+ * Click on the toggle to enable/disable autocomplete for IRI discovery<br>
+ * <img src="resources/guides/ttyg/configure-autocomplete-iri-discovery.png" style="height:200px; border: solid; border-width:1px"/><br>
+ *
+ * This step can be configured using the common options defined in [Options](#.Options). Additionally, it supports:
+ *
+ * @property {boolean} [options.disable] - Set to true to guide the user to disable the IRI discovery search, or false to enable it.
+ *
+ * @example
+ * ```JSON
+ * {
+ *  "guideBlockName": "configure-autocomplete-iri-discovery",
+ *  "options": {
+ *    "disable": true // or false to enable
+ *  }
+ * }
+ * ```
+ */
+const step = {
+  guideBlockName: 'configure-autocomplete-iri-discovery',
+  getSteps: function(options, pluginService) {
+    const GuideUtils = pluginService.GuideUtils;
+    const toggleSelector = GuideUtils.getGuideElementSelector('autocomplete-iri-discovery-input');
+    const shouldToggleOff = options.disable;
+    const translate = pluginService.translate;
+
+    let content;
+    if (shouldToggleOff) {
+      content = translate(this.translationBundle, DISABLE_TOGGLE);
+    } else {
+      content = translate(this.translationBundle, ENABLE_TOGGLE);
+    }
+
+    return {
+      guideBlockName: 'toggle-element',
+      options: {
+        content,
+        class: 'toggle-autocomplete-iri-discovery',
+        ...(options.title ?? {title: translate(this.translationBundle, TTYG_DEFAULT_TITLE)}),
+        ...options,
+        url: 'ttyg',
+        elementSelector: GuideUtils.getGuideElementSelector('autocomplete-iri-discovery'),
+        toggleableElementSelector: toggleSelector,
+        onNextValidate: () => {
+          const isEnabled = GuideUtils.isChecked(toggleSelector);
+          return Promise.resolve(shouldToggleOff ? !isEnabled : isEnabled);
+        }
+      }
+    };
+  },
+  translationBundle: {
+    en: {
+      [TTYG_DEFAULT_TITLE]: 'Talk to Your Graph',
+      [DISABLE_TOGGLE]: 'Click on the toggle to disable <b>Autocomplete for IRI discovery</b>.',
+      [ENABLE_TOGGLE]: 'Click on the toggle to enable <b>Autocomplete for IRI discovery</b>. This method helps the model discover relevant IRIs when a query contains incomplete names or references and cannot be resolved directly.'
+    },
+    fr: {
+      [TTYG_DEFAULT_TITLE]: 'Parlez à votre graphe',
+      [DISABLE_TOGGLE]: 'Cliquez sur le bouton pour désactiver l\'<b>Autocomplétion pour la découverte d\'IRI</b>.',
+      [ENABLE_TOGGLE]: 'Cliquez sur le bouton pour activer l\'<b>Autocomplétion pour la découverte d\'IRI</b>. Cette méthode aide le modèle à découvrir les IRI pertinents lorsqu\'une requête contient des noms ou des références incomplets et ne peut pas être résolue directement.'
+    }
+  }
+};
+
+export function register(registry) {
+  registry.add('guide.step', step);
+}
---
+++ plugins/guides/ttyg/set-max-iris-per-call.js
@@ -0,0 +1,66 @@
+const TTYG_DEFAULT_TITLE = 'menu.ttyg.label';
+const SET_MAX_IRIS_PER_CALL = 'guide.step_plugin.ttyg.set-max-iris-per-call';
+
+/**
+ * @name set-max-iris-per-call
+ * @memberof module:Interactive Guide
+ *
+ * @description
+ * This step guides the user to set the maximum number of IRIs to retrieve per call in the TTYG agent configuration.
+ * It highlights the input field and provides instructions on how to proceed.
+ *
+ * Set maximum IRIs per call<br>
+ * <img src="resources/guides/ttyg/set-max-iris-per-call.png" style="height:200px; border: solid; border-width:1px"/><br>
+ *
+ * This step can be configured using the common options defined in [Options](#.Options). Additionally, it supports:
+ *
+ * @property {number} options.maxIrisPerCall - The maximum number of IRIs per call. This will be displayed in the instructions and used for validation.
+ *
+ * @example
+ * ```JSON
+ * {
+ *  "guideBlockName": "set-max-iris-per-call",
+ *  "options": {
+ *    "maxIrisPerCall": 20
+ *  }
+ * }
+ * ```
+ */
+const step = {
+  guideBlockName: 'set-max-iris-per-call',
+  getSteps: function(options, pluginService) {
+    const GuideUtils = pluginService.GuideUtils;
+    const translate = pluginService.translate;
+    const maxIrisPerCall = options.maxIrisPerCall ?? 10;
+    const maxIrisInputSelector = GuideUtils.getGuideElementSelector('autocomplete-max-iris-input');
+
+    return {
+      guideBlockName: 'input-element',
+      options: {
+        content: translate(this.translationBundle, SET_MAX_IRIS_PER_CALL, {maxIrisPerCall}),
+        ...(options.title ?? {title: translate(this.translationBundle, TTYG_DEFAULT_TITLE)}),
+        class: 'set-max-iris-per-call',
+        ...options,
+        url: 'ttyg',
+        elementSelector: maxIrisInputSelector,
+        onNextValidate: () => {
+          return Promise.resolve(GuideUtils.validateTextInput(maxIrisInputSelector, maxIrisPerCall, false));
+        }
+      }
+    };
+  },
+  translationBundle: {
+    en: {
+      [TTYG_DEFAULT_TITLE]: 'Talk to Your Graph',
+      [SET_MAX_IRIS_PER_CALL]: 'Set the <b>Max number of results (IRIs) per call</b> to <b>{{maxIrisPerCall}}</b>.<br>This setting limits how many candidate IRIs the agent retrieves in a single autocomplete call when trying to resolve partial names. Keeping this value at a reasonable level helps control the amount of data returned so that the agent sees a manageable set of possible matches without overwhelming the query context.'
+    },
+    fr: {
+      [TTYG_DEFAULT_TITLE]: 'Parlez à votre graphe',
+      [SET_MAX_IRIS_PER_CALL]: 'Réglez le <b>Nombre maximal de résultats (IRI) par appel</b> sur <b>{{maxIrisPerCall}}</b>.<br>Ce paramètre limite le nombre d\'IRI candidates que l\'agent récupère lors d\'un seul appel d\'autocomplétion lorsqu\'il tente de résoudre des noms partiels. Maintenir cette valeur à un niveau raisonnable aide à contrôler la quantité de données renvoyées afin que l\'agent voie un ensemble gérable de correspondances possibles sans submerger le contexte de la requête.'
+    }
+  }
+};
+
+export function register(registry) {
+  registry.add('guide.step', step);
+}
